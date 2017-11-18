8544ae5d6a67962576d502b5940461c49da5b759
Doreen Wiggins <Wiggy77@aol.com>
2017-11-18 16:37:14 -0500

    updates

---
+++ gen.js/keys.js
@@ -0,0 +1,24 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'fhgskkjQcicDGctJPHq5ySjMC',
+  consumer_secret: 'nSSMVnhI8msf5ThcggYSf3vTvw1d3QkpQl6QgjnJF9FJ15daIa',
+  access_token_key: '930899174660460544-EnFtdwfZRqDBO1XjppOnhDKKO2bRFHM',
+  access_token_secret: 'WafA4HYsuLBCAbet5A1svpbi8LDGOtHwaGtlsfqWXDXSU',
+}
+
+module.exports = twitterKeys;
+
+
+// Access Token	930899174660460544-EnFtdwfZRqDBO1XjppOnhDKKO2bRFHM
+// Access Token Secret	WafA4HYsuLBCAbet5A1svpbi8LDGOtHwaGtlsfqWXDXSU
+// Access Level	Read and write
+// Owner	classaccount3
+// Owner ID	930899174660460544
+
+// Consumer Key (API Key)	fhgskkjQcicDGctJPHq5ySjMC
+// Consumer Secret (API Secret)	nSSMVnhI8msf5ThcggYSf3vTvw1d3QkpQl6QgjnJF9FJ15daIa
+// Access Level	Read and write (modify app permissions)
+// Owner	classaccount3
+// Owner ID	930899174660460544
+
