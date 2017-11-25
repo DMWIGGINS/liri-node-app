cdfc196ed352b37f7355cba41dffade51bfde642
Doreen Wiggins <Wiggy77@aol.com>
2017-11-24 21:50:45 -0500

    OMDB

--- gen.js/keys.js
+++ gen.js/keys.js
@@ -1,19 +1,27 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: 'fhgskkjQcicDGctJPHq5ySjMC',
-  consumer_secret: 'nSSMVnhI8msf5ThcggYSf3vTvw1d3QkpQl6QgjnJF9FJ15daIa',
+  consumer_key: '6gG5HmvxcrICzPCmj6qZO3lqj',
+  consumer_secret: 'ONYAi7JmNOLms7RVAh6da7np007wNGaPZsScE6iprp5yZU59X5',
   access_token_key: '930899174660460544-EnFtdwfZRqDBO1XjppOnhDKKO2bRFHM',
   access_token_secret: 'WafA4HYsuLBCAbet5A1svpbi8LDGOtHwaGtlsfqWXDXSU',
 };
 
 module.exports = twitterKeys;
 
-
+ 
 
 var spotify = {
   id: "c464dc23157e43708187577a737547f3",
-  secret: "e956a1b12da84cb4b4e6beabc4c09c47"
+  secret: "e956a1b12da84cb4b4e6beabc4c09c47",
 };
 
 module.exports = spotify;
+
+var movieKey = {
+  movie_key: "40e9cece"
+};
+
+module.exports = movieKey;
+
+// "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
--- gen.js/liri.js
+++ gen.js/liri.js
@@ -4,7 +4,6 @@ var T = new Twitter(client);
 
 if (process.argv[2] === "my-tweets") {
 
-
     var params = {
         screen_name: 'classaccount3',
         count: 20
@@ -17,46 +16,85 @@ if (process.argv[2] === "my-tweets") {
                 console.log(tweets[i].created_at);
                 console.log(tweets[i].text);
             }
+        } else {
+            return console.log(JSON.stringify(error));
 
         }
     });
 
 } else if (process.argv[2] === "spotify-this-song") {
+
+
     var Spotify = require('node-spotify-api');
     var secret = require('./keys.js');
     var song = new Spotify(secret);
 
+    // If no song is provided then default to "The Sign" by Ace of Base.
+    if (process.argv.length < 4) {
+        console.log("Ace of Base");
+        console.log("The Sign");
+        console.log("https://open.spotify.com/track/0hrBpAOgrt8RXigk83LLNE");
+        console.log("The Sign (US Album) [Remastered]");
+    } else {
 
-   
-    song.search({
-        type: 'track',
-        query: 'All the Small Things',
-        limit: 1
+        song.search({
+            type: 'track',
+            query: process.argv[3],
+            limit: 1
 
-    }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
 
-        // console.log(data);
-        // console.log(JSON.stringify(data));
+            } else {
+
+                console.log(data.tracks.items[0].artists[0].name);
+                console.log(data.tracks.items[0].name);
+                console.log(data.tracks.items[0].external_urls.spotify);
+                console.log(data.tracks.items[0].album.name);
+
+            }
+        });
+
+    }
+} else if (process.argv[2] === "movie-this") {
+    var request = require("request");
+    movieKey = require('./keys.js');
     
-        console.log(data.tracks.items[0].artists[0].name);
-        console.log(data.tracks.items[0].album.name);
-        console.log(data.tracks.items[0].name);
-        console.log(data.tracks.items[0].external_urls.spotify);
-        
-        
+
+    if (process.argv.length < 4) {
+        var movieName = "Mr. Nobody";
+    } else {
+        movieName = process.argv[3];
+    }
+
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + movieKey.movie_key;
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request was successful...
+        if (!error && response.statusCode === 200) {
+
+            // Then log the Release Year for the movie
+            console.log(JSON.parse(body).Title);
+            console.log(JSON.parse(body).Year);
+            console.log(JSON.parse(body).Ratings[0].Source);
+            console.log(JSON.parse(body).Ratings[0].Value);
+            console.log(JSON.parse(body).Ratings[1].Source);
+            console.log(JSON.parse(body).Ratings[1].Value);
+            console.log(JSON.parse(body).Country);
+            console.log(JSON.parse(body).Language);
+            console.log(JSON.parse(body).Plot);            
+            console.log(JSON.parse(body).Actors);
+        };
+        // * Title of the movie.response.title
+        // * Year the movie came out. response.year
+        // * IMDB Rating of the movie. response.ratings[0].source and response.ratings[0].value
+        // * Rotten Tomatoes Rating of the movie. response.ratings[1].source and response.ratings[1].value
+        // * Country where the movie was produced. response.country
+        // * Language of the movie. response.language
+        // * Plot of the movie. response.plot
+        // * Actors in the movie. response.actors
     });
 }
-
-
-// "href":"https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx"
-
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-
-
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
